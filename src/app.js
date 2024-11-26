1c3d452f5a3f024f12170b831541e0f56edb20f6
Maria Daniela Martin <m.danielamartin@proton.me>
2024-11-26 19:14:10 -0400

    basic working version completed

--- src/app.js
+++ src/app.js
@@ -7,22 +7,27 @@ let randomPick = function(lower, upper) {
 };
 let suits = [{ heart: "♥" }, { club: "♠" }, { spade: "♣" }, { diamond: "♦" }];
 let ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+
 let renderCard = function(suit, rank) {
   let suitKey = Object.keys(suit)[0];
   let symbols = document.querySelectorAll(".symbol");
+  let currentRank = document.querySelector(".rank");
+
   symbols.forEach(element => {
-    let currentSym = element.classList[1];
+    let currentSym = element.classList[2];
     element.classList.remove(currentSym);
     element.classList.add(suitKey);
     element.innerHTML = suit[suitKey];
   });
-  let currentRank = document.querySelector(".rank");
+
+  let rankColorSuit = currentRank.classList[1];
+  currentRank.classList.remove(rankColorSuit);
+  currentRank.classList.add(suitKey);
   currentRank.innerHTML = rank;
 };
 
 window.onload = function() {
   let suit = suits[randomPick(0, 3)];
   let rank = ranks[randomPick(0, 13)];
-
   renderCard(suit, rank);
 };
